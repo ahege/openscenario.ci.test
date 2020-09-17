a11656beadfe4d22e1bd55001760e8ce4893a32f
Ahege <a.hege@rac.de>
2020-09-17 14:38:16 +0200

    Initial

--- checkOscAction/entrypoint.sh
+++ checkOscAction/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -l
-java -jar /net.asam.openscenario.v1_0.reader-0.9.0-jar-with-dependencies.jar -d $GITHUB_WORKSPACE/$TARGET_FOLDER
+java -jar /net.asam.openscenario.v1_0.reader-0.9.0-jar-with-dependencies.jar -d $GITHUB_WORKSPACE/$CHECK_FOLDER
