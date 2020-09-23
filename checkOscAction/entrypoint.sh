9b0eae690b8706c5a9f5ee4e00b1d1b6f50b59b0
ahege <67583222+ahege@users.noreply.github.com>
2020-09-23 08:54:39 +0200

    Update entrypoint.sh

--- checkOscAction/entrypoint.sh
+++ checkOscAction/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh -l
-export CLASSPATH=/net.asam.openscenario.v1_0.reader-0.9.0-jar-with-dependencies.jar:$GITHUB_WORKSPACE/$INPUT_CHECKER_JAR_FILE
+if [[ -z "${$INPUT_CHECKER_JAR_FILE}" ]]; then
+  export CLASSPATH=/net.asam.openscenario.v1_0.reader-0.9.0-jar-with-dependencies.jar
+else
+  export CLASSPATH=/net.asam.openscenario.v1_0.reader-0.9.0-jar-with-dependencies.jar:$GITHUB_WORKSPACE/$INPUT_CHECKER_JAR_FILE
+fi
 sh -c "echo $CLASSPATH"
 java net.asam.openscenario.v1_0.main.OpenScenarioCiChecker  -conf $GITHUB_WORKSPACE/$INPUT_CONF_FILE -d $GITHUB_WORKSPACE/$INPUT_BASE_DIR
